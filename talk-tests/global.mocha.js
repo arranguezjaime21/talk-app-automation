54a9250b43d4e10c013f794f53236f2a9c85cba3
jhay arranguez <jhaymiee21@gmail.com>
2026-06-01 13:58:46 +0800

    mocha hooks global

---
+++ talk-tests/global.mocha.js
@@ -0,0 +1,22 @@
+import { remote } from "webdriverio";
+import { emulatorCaps } from "../helpers/capabilities.js";
+
+export const mochaHooks = { 
+    async beforeAll() {
+        this.timeout(12000);
+
+        global.driver = await remote({
+            path: "/",
+            port: 4723,
+            hostname: "127.0.0.1",
+            logLevel: "error",
+            capabilities: emulatorCaps
+        });
+    },
+
+    async afterAll() {
+        if(global.driver) { 
+            await global.driver.deleteSession();
+        }
+    }
+}
