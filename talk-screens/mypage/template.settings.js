836a93d0a508179d8edad606db0095e32a7c62c9
jhay arranguez <jhaymiee21@gmail.com>
2026-06-02 17:17:04 +0800

    template settings

--- talk-screens/mypage/template.settings.js
+++ talk-screens/mypage/template.settings.js
@@ -1,5 +1,5 @@
-import { CameraHelper } from "../../helpers/camera";
-import { MyPageSelectors, TemplateSelectors } from "../../talk-selectors/selectors";
+import { CameraHelper } from "../../helpers/camera.js";
+import { MyPageSelectors, TemplateSelectors } from "../../talk-selectors/selectors.js";
 import { BasePage } from "../base.js";
 
 export class TemplateSettings extends BasePage { 
@@ -11,98 +11,102 @@ export class TemplateSettings extends BasePage {
             ...TemplateSelectors
         };
 
-        this.cameraHelpers = new CameraHelper(
-            this.driver, 
+        this.cameraHelpers = new CameraHelper (
+            this.driver,
             this.waitAndClick.bind(this),
             this.waitAndFind.bind(this),
             this.waitAndFindAll.bind(this),
             this.elementExists.bind(this)
         );
+
     }
 
-    async navMyPageTemplate() { 
+    async navMyPageTemplate() {
         await this.waitAndClick(this.selectors.myPageNav);
         await this.waitAndClick(this.selectors.myPageTemplateSettings);
     }
 
-    async templateFiller({ description, mediaAction }) { 
+    async templateFiller({ description, mediaType }) { 
         if(!description?.trim()) { 
             throw new Error("Template description must not empty");
         }
 
         await this.setValue(this.selectors.templateDescription, description);
 
-        if(mediaAction) { 
-            await mediaAction(this.selectors);
+        if(mediaType) { 
+            await mediaType(this.selectors);
         }
 
         await this.elementExists(this.selectors.iconThumbImage, 5000);
     }
 
-    async createTemplate({ content, templateType = "test" }) { 
+    async createTemplate({ content, templateType = "text" }) {
         await this.waitAndClick(this.selectors.createTemplate);
 
         if(!content?.trim()) { 
             throw new Error("Template content must not empty");
         }
 
-        const template = { 
+        const template = {
             text: async() => {
                 await this.setValue(this.selectors.templateDescription, content);
-            }, 
+            },
             camera: async() => {
                 await this.templateFiller({ 
-                    description: content, 
-                    mediaAction: this.cameraHelpers.templateCameraCapture.bind(this.cameraHelpers),
+                    description: content,
+                    mediaType: this.cameraHelpers.templateCameraCapture.bind(this.cameraHelpers),
                 });
             },
             gallery: async() => {
                 await this.templateFiller({ 
                     description: content, 
-                    mediaAction: this.cameraHelpers.templateGallerySelection.bind(this.cameraHelpers),
+                    mediaType: this.cameraHelpers.templateGallerySelection.bind(this.cameraHelpers),
                 });
-            }
+            },
         };
 
         const action = template[templateType];
-
         if(!action) { 
-            throw new Error(`Incorrect inputted template type:
-                Inputted Template Type: ${templateType}, 
-                Valid Template Type: ${Object.keys(template).join(", ")}`);
+            throw new Error(`Invalid inputted template type: ${templateType}, 
+                Valid Inputs: ${Object.keys(template).join(", ")}`);
         }
 
         await action();
 
-        const templateDesc = await this.waitAndGetText(this.selectors.templateDescription);
-        await this.saveAndVerifyTemplate(templateDesc);
+        const createdTemplateDesc = await this.waitAndGetText(this.selectors.templateDescription);
+        await this.saveAndVerifyTemplate(createdTemplateDesc);
     }
 
-    async saveAndVerifyTemplate(expectedtext) { 
-        if(!expectedtext?.trim()) {
-            throw new Error("Template description must not empty");
+    async saveAndVerifyTemplate(expectedText) { 
+        if(!expectedText?.trim()) { 
+            throw new Error("Template expected description must not empty");
         }
-
         await this.waitAndClick(this.selectors.saveTemplate);
 
-        if(await this.elementExists(this.selectors.successModal, 3000)) { 
+        if(await this.elementExists(this.selectors.successModal, 5000)) { 
             await this.waitAndClick(this.selectors.confirmBtn);
         }
 
         await this.elementExists(this.selectors.templateList, 5000);
 
-        const templates = await this.safeFindAll(this.selectors.templateList, 5000);
-        if(!templates.length === 0) { 
-            throw new Error("No template found in the list , save template does not exists");
+        const templateList = await this.safeFindAll(this.selectors.templateList, 5000);
+        if(templateList.length === 0) { 
+            throw new Error("No template is displayed in the template list");
         }
 
-        const recentTemplate = templates[0];
+        const recentTemplate = templateList[0];
+
         const targetEl = await recentTemplate.$(this.selectors.postedText);
-        if(!(await targetEl.isExisting)) { 
+        if(!(await targetEl.isExisting())) { 
             throw new Error("Recent template has no description");
         }
 
-        const recentTemplateDesc = await targetEl.getText();
-        
+        const recentTemplateDesc = await targetEl.getText() || await targetEl.getAttribute("text");
+
+        if(recentTemplateDesc.trim() !== expectedText.trim()) { 
+            throw new Error(`Template display mismatch: 
+                Actual: ${recentTemplateDesc}, 
+                Expected: ${expectedText}`);
+        }
     }
 }
